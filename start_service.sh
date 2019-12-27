de75c956862788059b4b0c4c7e9d62404137afe4
theoneLee <623507020@qq.com>
2019-12-27 15:00:56 +0800

    fix shell usage

--- start_service.sh
+++ start_service.sh
@@ -1,4 +1,5 @@
 go build -o serviceBin service/service.go
 ./serviceBin $1 $2
 #usage ：
-# sh start_service.sh -addr localhost:8973
+# start the first node :  sh start_service.sh -addr localhost:8973
+# start the second : sh start_service.sh -addr localhost:8974
